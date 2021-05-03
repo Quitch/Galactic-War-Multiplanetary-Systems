a65fb2ae1841411a8acea769c3a9d02aff082828
Quitch <Quitch@users.noreply.github.com>
2021-05-03 13:49:58 +0100

    Setup structure of core JS file

---
+++ ui/mods/com.pa.quitch.galactic-war-multiplanetary-systems/load_maps.js
@@ -0,0 +1,14 @@
+var gwMultiplanetarySystemsLoaded;
+
+if (!gwMultiplanetarySystemsLoaded) {
+  gwMultiplanetarySystemsLoaded = true;
+
+  function gwMultiplanetarySystems() {
+    try {
+    } catch (e) {
+      console.error(e);
+      console.error(JSON.stringify(e));
+    }
+  }
+  gwMultiplanetarySystems();
+}
