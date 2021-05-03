62a5382fc3f93a401c906b09e6f8bd4c7956380e
Quitch <Quitch@users.noreply.github.com>
2021-05-03 14:05:00 +0100

    Create function to check map packs loaded
    Avoid generating errors trying to load files that aren't present

--- ui/mods/com.pa.quitch.galactic-war-multiplanetary-systems/load_maps.js
+++ ui/mods/com.pa.quitch.galactic-war-multiplanetary-systems/load_maps.js
@@ -5,6 +5,12 @@ if (!gwMultiplanetarySystemsLoaded) {
 
   function gwMultiplanetarySystems() {
     try {
+      api.mods.getMounted("client", true).then(function (mods) {
+        console.debug(mods);
+        var modPresent = function (modIdentifier) {
+          return _.some(mods, { identifier: modIdentifier });
+        };
+      });
     } catch (e) {
       console.error(e);
       console.error(JSON.stringify(e));
