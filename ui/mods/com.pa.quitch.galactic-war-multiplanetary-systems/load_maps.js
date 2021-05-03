c87a8ace9bef26c1924118f2d430e9a6ebb466fa
Quitch <Quitch@users.noreply.github.com>
2021-05-03 15:24:55 +0100

    Clarify variable name

--- ui/mods/com.pa.quitch.galactic-war-multiplanetary-systems/load_maps.js
+++ ui/mods/com.pa.quitch.galactic-war-multiplanetary-systems/load_maps.js
@@ -6,18 +6,18 @@ if (!gwMultiplanetarySystemsLoaded) {
   function gwMultiplanetarySystems() {
     try {
       api.mods.getMounted("client", true).then(function (mods) {
-        var modPresent = function (modIdentifier) {
+        var modMounted = function (modIdentifier) {
           return _.some(mods, { identifier: modIdentifier });
         };
 
         var multiplanetarySystems = [];
 
-        if (modPresent("com.pa.grandhomie.maps")) {
+        if (modMounted("com.pa.grandhomie.maps")) {
           multiplanetarySystems.push(
             "coui://ui/mods/grandhomie/systems/32_ffa.pas"
           );
         }
-        if (modPresent("nl.pa.wpmarshall.wpmarshall_map_pack")) {
+        if (modMounted("nl.pa.wpmarshall.wpmarshall_map_pack")) {
           multiplanetarySystems.push(
             "coui://ui/mods/wpmarshall/systems/(1v1)_ubersdkhto.pas",
             "coui://ui/mods/wpmarshall/systems/(5v5)_marshall's_lament (classic).pas",
@@ -26,7 +26,7 @@ if (!gwMultiplanetarySystemsLoaded) {
             "coui://ui/mods/wpmarshall/systems/(5v5)_world_of tomorrow 2.1.0.pas"
           );
         }
-        if (modPresent("nl.pa.Pwn4two.pwn4two_map_pack")) {
+        if (modMounted("nl.pa.Pwn4two.pwn4two_map_pack")) {
           multiplanetarySystems.push(
             "coui://ui/mods/pwn4two/systems/icecream_donut utensil.pas",
             "coui://ui/mods/pwn4two/systems/war_of the worlds.pas",
@@ -37,19 +37,19 @@ if (!gwMultiplanetarySystemsLoaded) {
             "coui://ui/mods/pwn4two/systems/dualdecahedron.pas"
           );
         }
-        if (modPresent("com.pa.academia.maps")) {
+        if (modMounted("com.pa.academia.maps")) {
           multiplanetarySystems.push(
             "coui://ui/mods/academia/systems/aca's_universe - s.pas",
             "coui://ui/mods/academia/systems/aca's_universe.pas"
           );
         }
-        if (modPresent("uk.pa.tetcmappack")) {
+        if (modMounted("uk.pa.tetcmappack")) {
           multiplanetarySystems.push(
             "coui://ui/mods/tetcmp1/systems/tg1v1.pas",
             "coui://ui/mods/tetcmp1/systems/saturn.pas"
           );
         }
-        if (modPresent("com.pa.sonicblue22.maps")) {
+        if (modMounted("com.pa.sonicblue22.maps")) {
           multiplanetarySystems.push(
             "coui://ui/mods/SonicBlue22/systems/destroyer.pas",
             "coui://ui/mods/SonicBlue22/systems/mega_asteroids.pas",
@@ -61,7 +61,7 @@ if (!gwMultiplanetarySystemsLoaded) {
             "coui://ui/mods/SonicBlue22/systems/wreck.pas"
           );
         }
-        if (modPresent("com.pa.dissonant.icarusmaps")) {
+        if (modMounted("com.pa.dissonant.icarusmaps")) {
           multiplanetarySystems.push(
             "coui://ui/mods/icarus/systems/anon/battle_of midway 5v5 ns.pas",
             "coui://ui/mods/icarus/systems/anon/intrepid_16 ffa.pas",
@@ -79,19 +79,19 @@ if (!gwMultiplanetarySystemsLoaded) {
             "coui://ui/mods/icarus/systems/anon/system-surge ds 3x3-3x5.pas"
           );
         }
-        if (modPresent("com.pa.kingslayergm.maps")) {
+        if (modMounted("com.pa.kingslayergm.maps")) {
           multiplanetarySystems.push(
             "coui://ui/mods/kingslayergm/systems/2v2v2_or_3v3v3_or_3_ffa_or_9_ffa_tripleplanet.pas",
             "coui://ui/mods/kingslayergm/systems/2v2v2v2_or_4v4_biplanet.pas",
             "coui://ui/mods/kingslayergm/systems/2v2v2v2v2_or_5_ffa_planetswar.pas"
           );
         }
-        if (modPresent("nl.pa.river.mappack")) {
+        if (modMounted("nl.pa.river.mappack")) {
           multiplanetarySystems.push(
             "coui://ui/mods/river/systems/dual_lake the system.pas"
           );
         }
-        if (modPresent("com.pa.therealf.maps")) {
+        if (modMounted("com.pa.therealf.maps")) {
           multiplanetarySystems.push(
             "coui://ui/mods/therealf/systems/xvx_or ffa orbital war.pas",
             "coui://ui/mods/therealf/systems/apx_system.pas",
@@ -102,7 +102,7 @@ if (!gwMultiplanetarySystemsLoaded) {
             "coui://ui/mods/therealf/systems/viroo.pas"
           );
         }
-        if (modPresent("zaktheevil.client.unified.mappack")) {
+        if (modMounted("zaktheevil.client.unified.mappack")) {
           multiplanetarySystems.push(
             "coui://ui/mods/zaktheevil/systems/fx211-system.pas",
             "coui://ui/mods/zaktheevil/systems/garum-system.pas",
